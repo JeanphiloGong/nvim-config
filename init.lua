ec8475e77f6ab6f2c7eb633727e3a48cffc6c816
Jeanphilo Gong <488034418@qq.com>
2025-04-08 15:53:41 +0800

    Add the Markdown Plugings

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = " "
 -- ~/.config/nvim/init.lua
 
 -- 初始化 lazy.nvim 插件管理器路径
@@ -23,4 +24,17 @@ require("lazy").setup("plugins")
 -- init.lua 配置（如果你用的是 Lua 配置）
 vim.opt.number = true         -- 显示绝对行号（当前行）
 vim.opt.relativenumber = true -- 其它行显示相对行号
+
+-- 自动优化 Markdown 编辑体验
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.spell = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
 
