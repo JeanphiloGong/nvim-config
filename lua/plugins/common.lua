50ded92410ad678ab71ebcccb8699215f0cc051a
gong <488034418@qq.com>
2025-12-08 15:05:07 +0800

    refactor: 对插件模块进行重构并添加前端emmet插件

---
+++ lua/plugins/common.lua
@@ -0,0 +1,204 @@
+-- 通用基础插件：补全、导航、编辑增强
+return {
+  -- 补全（cmp + LuaSnip）
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
+
+  -- 文件树
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        enable_git_status = true,
+        enable_diagnostics = true,
+        window = {
+          position = "left",
+          width = 30,
+          mappings = {
+            ["<space>"] = "toggle_node",
+            ["<cr>"] = "open",
+            ["P"] = "preview",
+            ["s"] = "open_split",
+            ["v"] = "open_vsplit",
+            ["a"] = "add",
+            ["d"] = "delete",
+            ["r"] = "rename",
+            ["R"] = "refresh",
+          },
+        },
+        filesystem = {
+          follow_current_file = { enabled = true },
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = true,
+          },
+        },
+        default_component_configs = {
+          git_status = {
+            symbols = {
+              added = "✚",
+              modified = "",
+              deleted = "✖",
+              renamed = "➜",
+              untracked = "★",
+              ignored = "◌",
+            },
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<CR>", { desc = "Toggle NeoTree" })
+    end,
+  },
+
+  -- 模糊搜索
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "master",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        defaults = {
+          prompt_prefix = "🔍 ",
+          selection_caret = "➤ ",
+          layout_strategy = "horizontal",
+          layout_config = { preview_width = 0.6 },
+        },
+      })
+      pcall(telescope.load_extension, "fzf")
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+      vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+      vim.keymap.set("n", "gr", builtin.lsp_references, {})
+      vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
+    end,
+  },
+
+  -- 成对包裹/替换
+  {
+    "kylechui/nvim-surround",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-surround").setup({
+        surrounds = {
+          ["t"] = {
+            add = function()
+              local tag = vim.fn.input("Tag: ")
+              return { "<" .. tag .. ">", "</" .. tag .. ">" }
+            end,
+          },
+        },
+      })
+    end,
+  },
+
+  -- tmux 分窗导航
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+
+  -- Markdown
+  {
+    "preservim/vim-markdown",
+    ft = { "markdown" },
+    config = function()
+      vim.g.vim_markdown_folding_disabled = 1
+      vim.g.vim_markdown_conceal = 0
+      vim.g.vim_markdown_toc_autofit = 1
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_theme = "dark"
+    end,
+  },
+  {
+    "mzlogin/vim-markdown-toc",
+    ft = { "markdown" },
+  },
+
+  -- which-key 提示
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup({})
+    end,
+  },
+
+  -- 行内注释
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  -- 自动补全括号/引号
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+
+  -- 缩进参考线
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = { char = "|" },
+    },
+  },
+}
